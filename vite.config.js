bb702ec0b74bf6ffcb93563861c8061ba9c05a4d
Work <work@Serhiis-MacBook-Pro.local>
2023-10-08 13:44:59 +0300

    set up deploy to gh-packages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteGhPages } from 'vite-plugin-gh-pages'
+import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteGhPages()],
+  plugins: [react(), Pages()],
+  build: {
+    outDir: 'dist',
+  },
 })
